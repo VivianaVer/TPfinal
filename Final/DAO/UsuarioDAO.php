6eaa738b74cc12b771d371030e78aa086d83db86
Viviana Vercesi <vivicver@gmail.com>
2022-12-13 17:36:11 -0300

    trabajo final

---
+++ Final/DAO/UsuarioDAO.php
@@ -0,0 +1,29 @@
+<?php 
+class UsuarioDAO {
+        public function validarUserYPass($user,$pass){
+            require_once("../dataBase/ConexionDB.php");
+            $conexionDB = new ConexionDB("localhost","root","","cacproyecto");
+            $conexionDB->conectar();
+
+            $sql = "SELECT * FROM usuarios WHERE usuario ='$user' and clave = '$pass'";
+            $conexionDB->ejecutar($sql);
+
+            return $ConexionDB->cantFilas() == 1;
+            }
+        public function listarUsuarios(){
+            require_once("../dataBase/ConexionDB.php");
+            require_once("../model/Usuario.php");
+            $conexionDB = new ConexionDB("localhost", "root", "", "cacproyecto");
+            $conexionDB->conectar();
+            $result = $conexionDB->ejecutar("SELECT * FROM usuarios");
+            
+            while($usuario=$result->fetch_assoc()){
+            $usuObj = new Usuario($usuario["idusuario"], $usuario["usuario"], $usuario["clave"]);
+            $listUsu[] = $usuObj;
+        }
+        return $listUsu; 
+        }
+    }
+    
+
+?>
---
+++ Final/view/listadoPedidos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+<link href="css/signin.css" rel="stylesheet">
+</head>
+<body>
+<header>
+        <nav>
+            <div id="header">
+                <ul class="nav">
+                    <li><a href="home.html">Inicio</a></li>
+                    <li><a href="">Usuarios</a>
+                        <ul>
+                            <li><a href="altaUsuarioC.html">Crear Usuario</a></li>
+                            <li><a href="listadoUsuarios.php">Listado de Usuarios</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">Pedidos</a>
+                        <ul>
+                            <li><a href="listadoPedidos.php">Listado de Pedidos</a></li>
+                            <li><a href="pedidoC.php">Cargar Pedido</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="">Acerca de</a>
+                        <ul>
+                            <li><a href="">Nosotros</a></li>
+                            <li><a href="">Nuestra Misión</a></li>
+                            <li><a href="http://www.facebook.com">Historia</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">Contacto</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+  
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
